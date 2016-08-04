2966b2d4bcbbad40d0762eeb4a22947ac1a880c9
Shea Newton <shnewto@gmail.com>
2016-08-03 23:31:16 -0700

    Changed git API page logic.
    Now provides link to commit with commit message.

--- github_api/commits.js
+++ github_api/commits.js
@@ -1,54 +1,52 @@
-// var userRequest = new XMLHttpRequest();
-// userRequest.open('GET', 'https://api.github.com/users/snewt' );
-//
-// userRequest.onreadystatechange = function() {
-//
-//     if( userRequest.readyState != 4 || userRequest.status != 200 ) {
-//         return;
-//     }
-//
-//     var userResponse = JSON.parse( userRequest.responseText) ;
-//
-//     var reposRequest = new XMLHttpRequest();
-//
-//     reposRequest.open('GET', userResponse.repos_url);
-//
-//     reposRequest.onreadystatechange = function() {
-//         console.log( reposRequest.responseText );
-//     };
-//
-//     console.log( 'GET: ', userResponse.repos_url );
-//
-//     reposRequest.send();
-// };
-//
-// console.log( 'GET: ', 'https://api.github.com/users/snewt' );
-//
-// userRequest.send();
 
-function printCommitMessages() {
-    var userResponse = JSON.parse(this.responseText);
 
-    for( var response of userResponse )
+var eventsRequest = new XMLHttpRequest();
+
+
+eventsRequest.open('GET', 'https://api.github.com/repos/snewt/ttaw-scratchpad/events' );
+
+
+eventsRequest.onload = parseCommits;
+
+
+function parseCommits()
+{
+    var eventsResponses = JSON.parse( this.responseText);
+
+    for( var response of eventsResponses )
     {
         if( response.type == "PushEvent" )
         {
             var commits = response.payload.commits;
-            for( var commit of commits )
+
+            for (var commit of commits)
             {
-                    document.getElementById( 'container' ).innerHTML +=
-                        '<br>' + '{' + '<br>' + commit.author.name + ': ' +
-                        '<br>' + commit.message + '<br>' + '}' + '<br>';
+                var urlRequest = new XMLHttpRequest();
+
+                urlRequest.open('GET', commit.url);
+
+                urlRequest.onload = parseURL;
+
+                urlRequest.send();
             }
         }
     }
-
-}
-
-var request = new XMLHttpRequest();
-request.onload = printCommitMessages;
-request.open('get', 'https://api.github.com/repos/snewt/symbolic-regression-c/events', true)
-request.send()
+};
 
 
+function parseURL()
+{
+    var urlResponse = JSON.parse( this.responseText );
+    document.getElementById( 'container' ).innerHTML +=
+        '<br>' + '{' + '<br>' +
+        '<a href=' +  urlResponse.html_url  + '>' +
+        urlResponse.sha.substr(0,9) +
+        '</a>' +
+        '<br>' + urlResponse.commit.message + '<br>' +
+        '}' + '<br>';
 
+    console.log( 'GET: ', urlResponse.html_url );
+};
+
+
+eventsRequest.send();
